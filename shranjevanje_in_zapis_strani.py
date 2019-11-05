edda23f1988ad979917b589d8c8daf501835f6d9
Jakob <jaka,zarnik@gmail.com>
2019-11-05 17:06:51 +0100

    Naredil csv

--- shranjevanje_in_zapis_strani.py
+++ shranjevanje_in_zapis_strani.py
@@ -5,8 +5,10 @@ import orodja
 
 url = 'https://www.studentska-prehrana.si/sl/restaurant'
 
-def shrani_stran():
+def shrani_stran(n):
     orodja.shrani_spletno_stran(url, 'restavracije.html')
+
+ime_datoteke='restavracije.html'
 
 def vsebina_datoteke(ime_datoteke):
     with open(ime_datoteke, 'r', encoding='utf-8') as r:
@@ -14,5 +16,32 @@ def vsebina_datoteke(ime_datoteke):
 
 def restavracije_loceno(ime_datoteke):
     vsebina_strani = vsebina_datoteke(ime_datoteke)
-    vzorec = r'<div class="row restaurant-row.*">$'
+    vzorec = (r'<div class="row restaurant-row(.*?)<div class="pull-right margin-right-10">')
     return re.findall(vzorec, vsebina_strani, flags=re.DOTALL)
+
+vzorec_podatkov = re.compile(
+    r'<div class="row restaurant-row'
+    r'.*?data-naslov="(?P<Naslov>.*?)"'
+    r'.*?data-cena="(?P<Cena>.*?)"'
+    r'.*?data-doplacilo="(?P<Doplacilo>.*?)"'
+    r'.*?data-posid="(?P<Posta>.*?)"'
+    r'.*?data-lokal="(?P<Ime_lokala>.*?)"'
+    r'.*?data-city="(?P<Mesto>.*?)"'
+    #r'(.*?<input checked="checked".*?value="(?P<Ocena>.*?)")?'
+    ,re.DOTALL
+    )
+vzorec_ocene = re.compile(r'checked="checked".*?value="(?P<ocena>.+?)".*?',
+                          re.DOTALL)
+
+
+def iskanje_podatkov_za_restavracije(ime_datoteke):
+    seznam_ponudb = vsebina_datoteke(ime_datoteke)
+    slovar_iskanih_podatkov = []
+    stevec = 0
+    for zadetek in re.finditer(vzorec_podatkov, seznam_ponudb):
+        slovar_iskanih_podatkov.append(zadetek.groupdict())
+        stevec +=1
+
+    orodja.zapisi_csv(slovar_iskanih_podatkov, ['Naslov', 'Mesto', 'Doplacilo', 'Posta', 'Cena', 'Ime_lokala'], 'restavracije.csv')
+
+    print(stevec)
